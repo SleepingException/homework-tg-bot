e94a4b1d3799293fc477c9236208aae9927070d7
TheGoodie <kaka.kalash@mail.ru>
2021-04-16 02:30:08 +0300

    modified data.sql

---
+++ src/main/resources/migrations/data.sql
@@ -0,0 +1,11 @@
+insert into "public".days("id","name", "date") values(1,'пн', '01-01-2021');
+insert into "public".days("id","name", "date") values(2,'вт', '01-01-2021');
+insert into "public".days("id","name", "date") values(3,'ср', '01-01-2021');
+insert into "public".days("id","name", "date") values(4,'чт', '01-01-2021');
+insert into "public".days("id","name", "date") values(5,'пт', '01-01-2021');
+insert into "public".days("id","name", "date") values(6,'сб', '01-01-2021');
+
+insert into "public".subjects (id, day_name, homework, time, info, name) values (1, 'пн', null, null, null, 'ИТЦП');
+insert into "public".subjects (id, day_name, homework, time, info, name) values (2, 'пн', null, null, null, 'Физ-ра');
+insert into "public".subjects (id, day_name, homework, time, info, name) values (3, 'пн', null, null, null, 'ВССиТ');
+insert into "public".subjects (id, day_name, homework, time, info, name) values (4, 'пн', null, null, null, 'ТСИС');
